f49739b17ff7b928958586e346e30ac6e7664a6d
VilaxDev <vilax.dev@gmail.com>
2025-03-03 19:53:51 -0500

    actualizacion de migracion

--- database/migrations/2025_03_04_000707_create_cache_table.php
+++ database/migrations/2025_03_04_000707_create_cache_table.php
@@ -11,12 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('cache', function (Blueprint $table) {
-            $table->string('key', 255)->primary();
-            $table->text('value');
-            $table->unsignedInteger('expiration');
-            $table->timestamps();
-        });
+       
     }
 
     /**
@@ -24,6 +19,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('cache');
+  
     }
 };
