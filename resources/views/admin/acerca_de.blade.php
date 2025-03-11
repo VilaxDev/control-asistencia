a915b917b838cf9fe7b91e1528fd6e1ce3ac678b
VilaxDev <vilax.dev@gmail.com>
2025-03-11 16:27:00 -0500

    Modificacion en el archivo acerca_de.blade.php

--- resources/views/admin/acerca_de.blade.php
+++ resources/views/admin/acerca_de.blade.php
@@ -3,22 +3,20 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12 text-center">
-                <h1 class="mb-4">Nuestros Valores</h1>
-                <h5>Misión</h5>
-                <p class="mt-4">
-                    Ofrecer soluciones tecnológicas innovadoras y personalizadas, involucrando a nuestros clientes en cada
-                    etapa del desarrollo para automatizar procesos, generar valor y alcanzar sus objetivos de manera
-                    colaborativa y sostenible.
-                </p>
-                <h5>Visión</h5>
-                <p class="mt-4">
-                    Ser una empresa tecnológica confiable y reconocida, avalada por las certificaciones más relevantes del
-                    sector, capaz de competir con las grandes empresas, adaptándonos continuamente a las nuevas demandas del
-                    mercado.
-                </p>
-                <p class="mt-4">
-                    Contáctanos para conocer más sobre cómo podemos ayudarte a llevar tu negocio al siguiente nivel.
-                </p>
+                <h1 class="mb-4">¡Descarga la Demo de nuestra Aplicación!</h1>
+                <p>Para utilizar completamente nuestro sistema de control de asistencia de personal, es necesario descargar
+                    nuestra aplicación APK. Esta versión de demostración te permitirá explorar todas las funcionalidades de
+                    nuestro sistema de manera fácil y rápida. Con ella, podrás gestionar el registro de asistencia de tu
+                    equipo de forma digital y eficiente.</p>
+                <p>Accede al siguiente enlace para descargar la demo de la aplicación directamente desde Google Drive. Sigue
+                    las instrucciones de instalación para empezar a probar el sistema. Te recomendamos descargar la última
+                    versión disponible para obtener las mejores prestaciones y funcionalidades.</p>
+                <a class="btn btn-primary"
+                    href="https://drive.google.com/drive/folders/1D5GRjmaOx05p5a3fjcWdQQAH0KKeU8PW?usp=drive_link">Descargar
+                    Demo APK
+                    <i class="ti ti-download"></i></a>
+                <p class="mt-3">Si tienes alguna duda durante la instalación o el uso de la aplicación, no dudes en ponerte
+                    en contacto con nosotros. ¡Estamos aquí para ayudarte a sacar el máximo provecho de la demo!</p>
             </div>
         </div>
     </div>
